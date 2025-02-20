4f08ec4c7cee9a3e65804606d006aab362d01783
EricYiming <ericeason2003@gmail.com>
2025-02-19 22:10:47 -0500

    deployment

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import axios from 'axios';
 import './App.css';
 
